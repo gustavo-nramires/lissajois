6fbf5cd0aa4d026396eb0ae5be019b22ffc8e8a2
gustavo-nramires <gustavo.nramires@gmail.com>
2020-09-02 05:02:10 -0300

    Mudança temporal de cor

--- lissajois.js
+++ lissajois.js
@@ -21,12 +21,12 @@ function liss(ifx,ify,iphase) {
     let p0y = sy(a,fy,t0,off);
     let p1x = sx(a,fx,t1,phase,off);
     let p1y = sy(a,fy,t1,off);
-    let c0r = 240;
+    let c0r = 255 * ((t+1) % 2 + 1)/3;
     let c0g = 0;
-    let c0b = 100;
+    let c0b = 0;//255 * ((t+0.5) % 2 + 1)/3;
     let c1r = 0;
-    let c1g = 255;
-    let c1b = 0;
+    let c1b = 255 * (t % 2 + 1)/3;
+    let c1g = 0;
     stroke(t0*c0r+(1-t0)*c1r,t0*c0g+(1-t0)*c1g,t0*c0b+(1-t0)*c1b);
     line(p0x,p0y,p1x,p1y);
   }
